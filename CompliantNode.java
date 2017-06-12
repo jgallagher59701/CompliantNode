1d09630b510406f84d97c75da7d29e506f656fcb
James Gallagher <jgallagher@opendap.org>
2017-06-11 22:50:09 -0600

    Starting on alg2

--- CompliantNode.java
+++ CompliantNode.java
@@ -11,28 +11,32 @@ public class CompliantNode implements Node {
 	
 	Set<Transaction> allTxs;	// The set of all transactions
 
-	HashMap<Transaction, Set<Integer>> newCandidates; // all candidate Txs received from followees
+	HashMap<Transaction, Set<Integer>> candidatesFromFollowee; // all Txs received from which followees
 	
+	HashMap<Integer, Set<Transaction>> followeeCandidates;	// all followees and the Txs they have sent
 
-	final int numRounds;
 	final double p_malicious;
 	final double p_txDistribution;
+	final int numRounds;
 	
 	int round;
 	int numFollowees;
 	int seenThreshold;
 	
+	boolean[] malicious;
+	
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
         // IMPLEMENT THIS
     	this.p_malicious = p_malicious;
-    	this.numRounds = numRounds;
     	this.p_txDistribution = p_txDistribution;
+
+    	this.numRounds = numRounds;
     	
     	round = 0;
     	numFollowees = 0;
     	seenThreshold = 0;
     	
-    	newCandidates = new HashMap<Transaction, Set<Integer>>();
+    	candidatesFromFollowee = new HashMap<Transaction, Set<Integer>>();
     }
 
     /** {@code followees[i]} is true if and only if this node follows node {@code i} */
@@ -45,10 +49,10 @@ public class CompliantNode implements Node {
     			++numFollowees;
     	}
     	
-    	seenThreshold = (int)(numFollowees * p_malicious);	// truncate decimal value
+    	seenThreshold = (int)((numFollowees - 1) * p_malicious);	// truncate decimal value
     	
-    	//System.err.println("Followees: " + numFollowees);
-    	//System.err.println("Seen Threshold: " + seenThreshold);
+    	System.err.println("Followees: " + numFollowees);
+    	System.err.println("Seen Threshold: " + seenThreshold);
     }
 
     /** initialize proposal list of transactions */
@@ -64,10 +68,18 @@ public class CompliantNode implements Node {
      */
     public Set<Transaction> sendToFollowers() {
         // IMPLEMENT THIS
+    	//return validTxs;
+    	
     	if (round < numRounds)
-    		return validTxs;
+    		return allTxs;
     	else {
-    		//System.err.println("returning valid Txs: " + validTxs.size() + ", allTxs: " + allTxs.size());
+			for (Transaction tx : candidatesFromFollowee.keySet()) {
+				Set<Integer> senders = candidatesFromFollowee.get(tx);
+				if (senders.size() >= seenThreshold) {
+					validTxs.add(tx);
+				}
+			}
+
     		return validTxs;
     	}
     }
@@ -76,61 +88,42 @@ public class CompliantNode implements Node {
     public void receiveFromFollowees(Set<Candidate> candidates) {
         // IMPLEMENT THIS
     	++round;
-    	
-    	// A Candidate transaction is valid if its id is in the list of validTxs
-    	// build by the simulator (which this node does not know). 
-    	//
-    	// A Candidate Tx is valid if:
-    	// it's Hash matches that of a known valid Tx
-    	// two or more nodes have proposed it 
-    	// (or some fraction of the followees proposed it)
-    	
+    	    	
     	// Alg0. Everything is good if it comes from a followee
+    	/*
     	for (Candidate c: candidates) {
     		if (followees[c.sender])
     			validTxs.add(c.tx);
     	}
-    	
-    	//
+    	*/
+
     	// Alg1. 
     	// The initial Txs are all valid
     	// A candidate Tx is held in a buffer if it's not on the valid list
-    	// and it has only been seen from one followee.
     	// A candidate from some threshold number of followees is assumed valid
     	// Only Txs assumed valid are sent to followers
-    	/*
+    	
     	for (Candidate c: candidates) {
     		if (!followees[c.sender])
     			continue;
     		
-    		// if this candidate tx on the validTxs list, leave it there.
-    		// if not, add it to the newTxs list
-    		if (!validTxs.contains(c.tx)) {
-    			Set<Integer> senders = newCandidates.get(c.tx);
-    			if (senders == null) {
-    				senders = new HashSet<Integer>();
-    				senders.add(c.sender);
-    				newCandidates.put(c.tx, senders);
-    			}
-    			else if (!senders.contains(c.sender)) {
-    				senders.add(c.sender);
-    				newCandidates.put(c.tx, senders);
-    			}
-    		}
-    	}
+    		allTxs.add(c.tx);		// Send along every tx we get
+    		
+    		// Record all inbound Txs along with the number of nodes that have sent them
+			Set<Integer> senders = candidatesFromFollowee.get(c.tx);
+			if (senders == null) {
+				senders = new HashSet<Integer>();
+				senders.add(c.sender);
+				candidatesFromFollowee.put(c.tx, senders);
+			} else if (!senders.contains(c.sender)) {
+				senders.add(c.sender);
+				candidatesFromFollowee.put(c.tx, senders);
+			}
+		}
     	
-    	// Test for candidate Txs from more than one other node 
-    	for (Transaction tx: newCandidates.keySet()) {
-    		Set<Integer> senders = newCandidates.get(tx);
-    		if (senders.size() >= seenThreshold) {
-    			allTxs.add(tx);
-    			validTxs.add(tx);
-    		}
-    		else if (senders.size() >= (int)(seenThreshold * p_txDistribution)) {
-    			// The difference between this and broadcasting all txs was nil
-        		allTxs.add(tx);
-    		}
-    	}
-    	*/
-    }
+    	// Alg2. Identify malicious nodes
+    	// A node that does not send any Txs in the first round is malicious
+    	
+	}
+    
 }
